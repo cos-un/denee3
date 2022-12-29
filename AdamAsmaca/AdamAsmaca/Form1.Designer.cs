c3c66f678e3dbbc5ee5f3922b67a9e06dd86cda1
cos-un <cosgunmehmet93@gmail.com>
2022-12-29 23:26:03 +0300

    deneme

---
+++ AdamAsmaca/AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBasla = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonBul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(289, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonBasla
+            // 
+            this.buttonBasla.Location = new System.Drawing.Point(38, 90);
+            this.buttonBasla.Name = "buttonBasla";
+            this.buttonBasla.Size = new System.Drawing.Size(86, 66);
+            this.buttonBasla.TabIndex = 7;
+            this.buttonBasla.Text = "Başla";
+            this.buttonBasla.UseVisualStyleBackColor = true;
+            this.buttonBasla.Click += new System.EventHandler(this.buttonBasla_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(289, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // buttonBul
+            // 
+            this.buttonBul.Location = new System.Drawing.Point(360, 190);
+            this.buttonBul.Name = "buttonBul";
+            this.buttonBul.Size = new System.Drawing.Size(116, 48);
+            this.buttonBul.TabIndex = 10;
+            this.buttonBul.Text = "Bul";
+            this.buttonBul.UseVisualStyleBackColor = true;
+            this.buttonBul.Click += new System.EventHandler(this.buttonBul_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 554);
+            this.Controls.Add(this.buttonBul);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonBasla);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBasla;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonBul;
+    }
+}
+
---
+++ AdamAsmaca/AdamAsmaca/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] kelimeler = { "ARABA", "CAM", "KELEBEK", "TELEFON", "MÜZİK", "FONT", "CIRAK", "CEZA", "FARE", "HALAT", };
+        String secim;
+        String kelime;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        } 
+
+        private void buttonBasla_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            secim = kelimeler[rnd.Next(1, 7)];
+            label8.Text = secim;
+            label1.Text = "";
+            for (int i = 0; i < secim.Length; i++)
+            {
+                kelime += " _ ";
+            }
+            label1.Text = kelime;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBul_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
